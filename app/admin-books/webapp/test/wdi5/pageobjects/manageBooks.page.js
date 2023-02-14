980970f1d2f55147bcd1cb3e7f1e7d36187d54af
scoen <simon.coen@js-soft.com>
2023-02-14 16:23:39 +0100

    wip: chromedriver test

--- app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
@@ -2,8 +2,30 @@ module.exports = {
   /**
     * define actions for the page object
   */
-
+  iClickOnCreateNewBook: async () => {
+    await browser.asControl({
+      selector: {
+        id: "books::BooksList--fe::table::Books::LineItem::StandardAction::Create",
+        interaction: {
+          idSuffix: "BDI-content"
+        }
+      }
+    }).press();
+  },
   /**
    * define assertions for the page object
    */
+
+  theBookListContains: async (sBook) => {
+    const visible = await browser.asControl({
+      selector: {
+        controlType: "sap.m.Text",
+        viewId: "books::BooksList",
+        properties: {
+          text: sBook
+        }
+      }
+    }).getVisible();
+    expect(visible).toBeTruthy();
+  }
 }
